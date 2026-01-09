ba7388e019f34cc9886aaa0ac3de1f9d4ee5cdfa
murshi . <murshi.@murshis-MacBook-Air.local>
2026-01-09 15:54:41 +0530

    created order page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router";
 import { HomePage } from "./pages/HomePage";
 import { CheckoutPage } from "./pages/CheckoutPage";
+import { Orders } from "./pages/Orders";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<Orders />} />
     </Routes>
   );
 }
