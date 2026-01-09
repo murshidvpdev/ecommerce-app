d881b47cb2863afd1c552aa478897a8eeb013e9e
murshi . <murshi.@murshis-MacBook-Air.local>
2026-01-09 16:17:28 +0530

    added tracking page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router";
 import { HomePage } from "./pages/HomePage";
 import { CheckoutPage } from "./pages/CheckoutPage";
 import { Orders } from "./pages/Orders";
+import { Tracking } from "./pages/Tracking";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
       <Route path="orders" element={<Orders />} />
+      <Route path="tracking" element={<Tracking />} />
     </Routes>
   );
 }
