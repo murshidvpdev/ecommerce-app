b4ce287ad7a7b0b7313de8d78f6121180e646048
murshi . <murshi.@murshis-MacBook-Air.local>
2026-01-09 15:07:37 +0530

    checkout page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { HomePage } from './pages/HomePage'
-import './App.css'
+import { Routes, Route } from "react-router";
+import { HomePage } from "./pages/HomePage";
+import { CheckoutPage } from "./pages/CheckoutPage";
+import "./App.css";
 
 function App() {
   return (
-    <>
-      < HomePage/>
-    </>
-  )
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
